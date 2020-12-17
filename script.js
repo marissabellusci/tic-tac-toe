cdbaef9559fca3ba4f3a3c4560868ae793fdf1a8
Marissa <marissabellusci@gmail.com>
2020-12-17 13:50:22 -0700

    added logic to keep from selecting a space that is already filled & added an error message

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//module
 const gameBoard = (() => {
     'use strict';
 
@@ -6,18 +7,43 @@ const gameBoard = (() => {
     return boardArray;
 })();
 
-const displayController = (() => {
+//fcty function
+const displayController = (pos,y) => {
     'use strict';
-    console.log('display controls go here!')
 
-})();
+    document.getElementById("error-message").textContent = '';
+    
+    if (gameBoard[pos] == ' '){
+    gameBoard.splice(pos, 1, y);
+    console.log(gameBoard[pos]);
+    }
+    else {
+    document.getElementById("error-message").textContent = "There's already something in that spot!";
+    return console.log("There's already something in that spot!");
+    }
 
-document.getElementById('cell-0').textContent = gameBoard[0];
-document.getElementById('cell-1').textContent = gameBoard[1];
-document.getElementById('cell-2').textContent = gameBoard[2];
-document.getElementById('cell-3').textContent = gameBoard[3];
-document.getElementById('cell-4').textContent = gameBoard[4];
-document.getElementById('cell-5').textContent = gameBoard[5];
-document.getElementById('cell-6').textContent = gameBoard[6];
-document.getElementById('cell-7').textContent = gameBoard[7];
-document.getElementById('cell-8').textContent = gameBoard[8];
+    //RENDERS EVERYTHING TO THE DOM//
+        function refreshPage(){
+            document.getElementById('cell-0').textContent = gameBoard[0];
+            document.getElementById('cell-1').textContent = gameBoard[1];
+            document.getElementById('cell-2').textContent = gameBoard[2];
+            document.getElementById('cell-3').textContent = gameBoard[3];
+            document.getElementById('cell-4').textContent = gameBoard[4];
+            document.getElementById('cell-5').textContent = gameBoard[5];
+            document.getElementById('cell-6').textContent = gameBoard[6];
+            document.getElementById('cell-7').textContent = gameBoard[7];
+            document.getElementById('cell-8').textContent = gameBoard[8];
+            };
+
+    refreshPage();
+
+    //LISTENS FOR CLICKS//
+
+    return gameBoard;
+
+};
+
+
+
+
+
