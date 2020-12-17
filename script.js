3f1d12ab927b7fcb3cb052572a5498c2cdf15371
Marissa <marissabellusci@gmail.com>
2020-12-17 14:26:42 -0700

    cleaned up some console.logs and initialized a player module

--- script.js
+++ script.js
@@ -2,46 +2,55 @@
 const gameBoard = (() => {
     'use strict';
 
+    document.querySelectorAll('.cell')
+    .forEach(cell => {cell.addEventListener('mouseup', function(event){
+        const attack = event.target.value;
+        game(attack,'x');
+    })});
+
     const boardArray = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
-    console.table(boardArray);
+
     return boardArray;
 })();
 
 //fcty function
-const displayController = (pos,y) => {
+const game = (pos,y) => {
     'use strict';
 
     document.getElementById("error-message").textContent = '';
     
     if (gameBoard[pos] == ' '){
     gameBoard.splice(pos, 1, y);
-    console.log(gameBoard[pos]);
     }
+
     else {
     document.getElementById("error-message").textContent = "There's already something in that spot!";
-    return console.log("There's already something in that spot!");
     }
 
     //RENDERS EVERYTHING TO THE DOM//
-        function refreshPage(){
-            document.getElementById('cell-0').textContent = gameBoard[0];
+        (function refreshPage(){
             document.getElementById('cell-1').textContent = gameBoard[1];
             document.getElementById('cell-2').textContent = gameBoard[2];
+            document.getElementById('cell-0').textContent = gameBoard[0];
             document.getElementById('cell-3').textContent = gameBoard[3];
             document.getElementById('cell-4').textContent = gameBoard[4];
             document.getElementById('cell-5').textContent = gameBoard[5];
             document.getElementById('cell-6').textContent = gameBoard[6];
             document.getElementById('cell-7').textContent = gameBoard[7];
             document.getElementById('cell-8').textContent = gameBoard[8];
-            };
 
-    refreshPage();
+            console.table(gameBoard);
+            })();
 
-    //LISTENS FOR CLICKS//
+
 
     return gameBoard;
 
 };
+
+const player = (() => {
+    return console.log('new player!')
+});
 
 
 
