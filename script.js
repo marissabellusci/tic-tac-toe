ea328fb92955ae4dfb554b1886cb04ae16f7e75f
Marissa <marissabellusci@gmail.com>
2020-12-17 11:47:31 -0700

    css for x/o's and js to render array to the dom

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const gameBoard = (() => {
     'use strict';
 
-    const boardArray = ['x','0','x','0','x','0','x','0','x'];
+    const boardArray = ['x','O','x','O','x','O','x','O','x'];
     console.table(boardArray);
+    return boardArray;
 })();
 
 const displayController = (() => {
@@ -10,3 +11,13 @@ const displayController = (() => {
     console.log('display controls go here!')
 
 })();
+
+document.getElementById('cell-0').textContent = gameBoard[0];
+document.getElementById('cell-1').textContent = gameBoard[1];
+document.getElementById('cell-2').textContent = gameBoard[2];
+document.getElementById('cell-3').textContent = gameBoard[3];
+document.getElementById('cell-4').textContent = gameBoard[4];
+document.getElementById('cell-5').textContent = gameBoard[5];
+document.getElementById('cell-6').textContent = gameBoard[6];
+document.getElementById('cell-7').textContent = gameBoard[7];
+document.getElementById('cell-8').textContent = gameBoard[8];
