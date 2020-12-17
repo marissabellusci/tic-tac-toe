4a67e9c79e9f078d9fda193f49401b34fe0150e6
Marissa <marissabellusci@gmail.com>
2020-12-17 10:52:31 -0700

    initialized gameBoard and displayController modules

---
+++ script.js
@@ -0,0 +1,12 @@
+const gameBoard = (() => {
+    'use strict';
+
+    const boardArray = ['x','0','x','0','x','0','x','0','x'];
+    console.table(boardArray);
+})();
+
+const displayController = (() => {
+    'use strict';
+    
+
+})();
